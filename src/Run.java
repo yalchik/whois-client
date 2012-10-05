a1d7d60281f01a56e03720770e6839ab6348c4b3
Yalchik <yalchik.ilya@gmail.com>
2012-10-05 21:51:32 +0300

    Работает, кажется)

--- src/Run.java
+++ src/Run.java
@@ -1,12 +1,21 @@
-
 public class Run {
-
+	
 	/**
-	 * @param args
+	 * Entry point
+	 * @param args arguments
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		if (args.length == 2) {
+			Whois whois = new Whois();
+			whois.ask(args[0], args[1]);
+		}
+		else
+			System.out.println(String.format("Error: %s\nFormat: %s\nExample: %s",
+					"Wrong input",
+					"java -jar [filename] [server] [domain]",
+					"java -jar aipos2.jar whois.nic.ru yandex.ru"
+					));
+		
 	}
-
 }
