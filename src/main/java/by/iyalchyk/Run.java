cbd308d9a05ed7c38a6990cb0734ad6033f95a1d
iyalchyk <yalchik.ilya@gmail.com>
2018-06-24 00:19:00 +0300

    cleaned up redundant files, actualized and improved the code

---
+++ src/main/java/by/iyalchyk/Run.java
@@ -0,0 +1,46 @@
+package by.iyalchyk;
+
+import org.apache.commons.validator.routines.DomainValidator;
+
+import java.io.IOException;
+
+public class Run {
+
+    private static final DomainValidator domainValidator = DomainValidator.getInstance(true);
+
+	/**
+	 * Entry point
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println(String.format("Error: %s\nFormat: %s\nExample: %s",
+                    "Wrong input",
+                    "java -jar [filename] [server] [domain]",
+                    "java -cp target/WhoisClient-1.0-SNAPSHOT.jar by.iyalchyk.Run whois.nic.ru yandex.ru"
+            ));
+            System.exit(1);
+        }
+
+        String serverDomain = args[0];
+        if (!domainValidator.isValid(serverDomain)) {
+            System.out.println("Error: whois-server domain is incorrect. Please make sure you do not specify a protocol (e.g. http://server.com). Specify just the domain (e.g. server.com");
+            System.exit(2);
+        }
+
+        String requestDomain = args[1];
+        if (!domainValidator.isValid(requestDomain)) {
+            System.out.println("Error: request domain is incorrect. Please make sure you do not specify a protocol (e.g. http://server.com). Specify just the domain (e.g. server.com");
+            System.exit(3);
+        }
+
+        try {
+            String whoisResponse = WhoisClient.askOneTime(serverDomain, requestDomain);
+            System.out.println(whoisResponse);
+        } catch (IOException e) {
+            System.out.println("Error: network connection issues");
+            System.exit(4);
+        }
+	}
+
+}
---
+++ src/main/java/by/iyalchyk/WhoisClient.java
@@ -0,0 +1,123 @@
+package by.iyalchyk;
+
+import org.apache.commons.text.TextStringBuilder;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Requests information on a given domain from a given whois-server via WhoisClient protocol (see RFC 954)
+ * @author Ilya Yalchyk
+ */
+public class WhoisClient {
+	
+	/** Default port for whois protocol */
+	public final int DEFAULT_PORT = 43;
+
+	/** Default whois-server domain */
+    public final String DEFAULT_SERVER = "whois.nic.ru";
+
+	/** Socket for whois-server connection */
+	private Socket socket = null;
+	
+	/** Writes data to whois-server connection */
+	private PrintWriter writer = null;
+	
+	/** Reads data from whois-server connection */
+	private BufferedReader reader = null;
+	
+	/**
+	 * Connects to the given whois-server
+	 * @param serverDomain whois-server domain
+	 * @throws IOException If an I/O error occurs during the operation.
+	 */
+	public void connect(String serverDomain) throws IOException {
+	    try {
+            socket = new Socket(serverDomain, DEFAULT_PORT);
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+	        disconnect();
+	        throw e;
+        }
+	}
+
+    /**
+     * Connects to the default whois-server
+     * @throws IOException If an I/O error occurs during the operation.
+     */
+	public void connect() throws IOException {
+        connect(DEFAULT_SERVER);
+    }
+	
+	/**
+	 * Sends query to the connected whois-server
+	 * @param domain domain to request from the whois-server
+	 * @throws IOException If an I/O error occurs during the operation.
+	 */
+	public String queryText(String domain) throws IOException {
+        String buffer;
+        TextStringBuilder sb = new TextStringBuilder();
+		writer.println(domain);
+
+		while ((buffer = reader.readLine()) != null) {
+            sb.appendln(buffer);
+        }
+		return sb.build();
+	}
+	
+	/**
+	 * Closes streams and socket
+	 */
+	public void disconnect() {
+        silentClose(reader);
+        silentClose(writer);
+        silentClose(socket);
+	}
+
+	/**
+	 * Facade for single query
+	 * @param serverDomain URL of whois-server
+	 * @param requestDomain Requested domain
+	 */
+	public static String askOneTime(String serverDomain, String requestDomain) throws IOException {
+        WhoisClient whoisClient = new WhoisClient();
+        whoisClient.connect(serverDomain);
+        String whoisResponse;
+        try {
+            whoisResponse = whoisClient.queryText(requestDomain);
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            whoisClient.disconnect();
+        }
+        return whoisResponse;
+	}
+
+
+    private void silentClose(Socket socket) {
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // Do nothing
+            }
+        }
+    }
+
+    private void silentClose(Closeable stream) {
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                // Do nothing
+            }
+        }
+    }
+
+}
